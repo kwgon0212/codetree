1b7917baa520e9930edaf40ab6dd799b3058535f
U_goni <86401186+kwgon0212@users.noreply.github.com>
2024-10-27 17:51:26 +0900

    Title: 길이 단위 변환하기, Time: 158ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 241027/길이 단위 변환하기/change-length-unit.js
@@ -0,0 +1,8 @@
+const ft = 30.48
+const mi = 160934
+
+const first = 9.2
+const second = 1.3
+
+console.log(`${first}ft = ${(first * ft).toFixed(1)}cm`)
+console.log(`${second}mi = ${(second * mi).toFixed(1)}cm`)
